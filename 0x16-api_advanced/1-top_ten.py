acb4da0f759194d20126fae09131b8ccfe48455b
ambrose <kolus7381@gmail.com>
2023-11-07 21:26:28 +0100

    ew

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ import requests
 def top_ten(subreddit):
     """shows titles of the first 10 hot posts for reddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    req = requests.get(url, headers={'User-Agent': 'Alexapp/1.0'})
+    req = requests.get(url)
     if req.status_code == 200:
         data = req.json()
         titles = [item["data"]["title"] for item in data["data"]["children"]]
