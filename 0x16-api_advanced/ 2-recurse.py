5abaab7f7b25045d5e8a21443b128699465a2c11
ambrose <kolus7381@gmail.com>
2023-11-07 21:41:34 +0100

    ew

--- 0x16-api_advanced/ 2-recurse.py
+++ 0x16-api_advanced/ 2-recurse.py
@@ -1,54 +1,28 @@
 #!/usr/bin/python3
-"""
-This is a program that works with the Reddit API
-It prints the titles of the first 10 hot posts
-"""
-
-import requests
+"""Module for task 2"""
 
 
-def recurse(subreddit, hot_list=None, after=None):
-    # Initialize hot_list as an empty list on the first call
-    if hot_list is None:
-        hot_list = []
+def recurse(subreddit, hot_list=[], count=0, after=None):
+    """Queries the Reddit API and returns all hot posts
+    of the subreddit"""
+    import requests
 
-    # Define the Reddit API endpoint for the subreddit's hot posts
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-
-    # Include 'after' parameter to paginate through results
-    if after:
-        url += f"&after={after}"
-
-    # Set a custom User-Agent header to avoid API access issues
-    headers = {'User-Agent': 'MyRedditApp/1.0'}
-
-    # Make the GET request to the API
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the response is valid (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON data from the response
-        data = response.json()
-        posts = data.get('data', {}).get('children', [])
-
-        if not posts:
-            # If no posts are found, return None
-            return None
-        else:
-            # Add the titles of the current page of hot posts to the hot_list
-            for post in posts:
-                hot_list.append(post['data']['title'])
-
-            # Recursively call the function with the 'after'
-            next_page = data.get('data', {}).get('after')
-            if next_page:
-                return recurse(subreddit, hot_list, next_page)
-            else:
-                return hot_list
-    elif response.status_code == 302:
-        # If the subreddit is invalid and results in a redirect, return None
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
+                            params={"count": count, "after": after},
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 400:
         return None
-    else:
-        # Handle other possible errors if needed
-        print(f"Error: {response.status_code}")
-        return None
+
+    hot_l = hot_list + [child.get("data").get("title")
+                        for child in sub_info.json()
+                        .get("data")
+                        .get("children")]
+
+    info = sub_info.json()
+    if not info.get("data").get("after"):
+        return hot_l
+
+    return recurse(subreddit, hot_l, info.get("data").get("count"),
+                   info.get("data").get("after"))
